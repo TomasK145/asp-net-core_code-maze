de7eb3e88ed8116e439c58901f9931c42a40130f
Tomas Krchnak <tomas.krchnak@millennium.sk>
2019-08-30 14:12:22 +0200

    https://code-maze.com/views-partial-views-and-layouts-in-asp-net-core-mvc/ - Using Razor Markup to build Views - Reusing sections of pages using Partial Views - Creating a common look and feel for the application using Layout files

--- NetCoreCodeMazePart2WorkingWithData/Controllers/BooksController.cs
+++ NetCoreCodeMazePart2WorkingWithData/Controllers/BooksController.cs
@@ -42,28 +42,6 @@ namespace NetCoreCodeMazePart2WorkingWithData.Controllers
             return View(book);
         }
 
-        // GET: Books/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Books/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Title,Genre,Price,PublishDate")] Book book)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(book);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(book);
-        }
-
         // GET: Books/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
@@ -114,36 +92,7 @@ namespace NetCoreCodeMazePart2WorkingWithData.Controllers
             }
             return View(book);
         }
-
-        // GET: Books/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var book = await _context.Book
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (book == null)
-            {
-                return NotFound();
-            }
-
-            return View(book);
-        }
-
-        // POST: Books/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var book = await _context.Book.FindAsync(id);
-            _context.Book.Remove(book);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
+ 
         private bool BookExists(int id)
         {
             return _context.Book.Any(e => e.Id == id);
